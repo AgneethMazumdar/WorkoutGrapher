e0609a79abac63e4bea1323287ac5ecccd8ed0c7
Agneeth Mazumdar <agneeth.mazumdar@gmail.com>
2015-09-10 13:54:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ import matplotlib.dates as mdates
 import seaborn as sns
 import numpy as np
 
+
 class Exercises(object):
+
 
     def __init__(self, name):
         self.name = name
@@ -27,15 +29,21 @@ class Exercises(object):
     def addReps(self, reps):
         self.reps.append(reps)
 
+"""
+Create a list of exercises, dates, and exercise objects. Reading the data from
+the csv file and making it searchable will require reformatting the data into
+its individuals components later on (weight, sets, reps, and dates).
+"""
+
 def create_exercise_objects():
 
     exercises, dates = [], []
 
-    with open('WorkoutData.csv', 'rb') as workout_data: 
+    with open('WorkoutData.csv', 'rb') as workout_data:
         csv_workout_data = csv.reader(workout_data)
 
         for index, row in enumerate(csv_workout_data):
-            
+
             dates.append(row[0])
 
             if index == 0:
@@ -48,23 +56,32 @@ def create_exercise_objects():
 
     return exercises, exercise_names, dates
 
+"""
+Bind the column of workouts with their respective exercises.
+"""
+
 def make_wsr_dictionary(counter, names):
 
-    temp, catalog = [], {}
+    catalog = {}
 
-    with open('WorkoutData.csv', 'rb') as workout_data: 
+    with open('WorkoutData.csv', 'rb') as workout_data:
         csv_workout_data = csv.reader(workout_data)
 
         try:
-            for index, row in enumerate(csv_workout_data):
-                temp.append(row[counter+1])
-
+            temp = [row[counter+1] for index, row in enumerate(csv_workout_data)]
             catalog[names[counter]] = temp[1:]
             temp = []
+
         except:
             pass
 
     return catalog
+
+"""
+Parse the individual cells and get rid of the spaces so that it's easier
+to separate the weight, sets, and reps. Also make sure that empty cells
+will not cause runtime errors by filling them in with zeros.
+"""
 
 def start_filtering(full_catalog):
 
@@ -73,18 +90,20 @@ def start_filtering(full_catalog):
 
             value[index] = string.replace(value[index], ' ', 'x')
             value[index] = value[index].split('x')
-            
+
             if len(value[index]) < 3:
                 value[index] = [0, 0, 0]
 
     return full_catalog
 
+"""
+Add the weight, sets, and reps attributes to its respective exercise attribute.
+"""
+
 def append_attributes(exercises, full_catalog, names):
 
     for index, value in enumerate(names):
-        full_catalog[names[index]]=  map(list, zip(*full_catalog[names[index]]))
-
-    for index, value in enumerate(names):
+        full_catalog[names[index]] = map(list, zip(*full_catalog[names[index]]))
         weights_sets_reps = full_catalog[names[index]]
 
         exercises[index].addWeight(map(int, weights_sets_reps[0]))
@@ -93,13 +112,36 @@ def append_attributes(exercises, full_catalog, names):
 
     return exercises
 
+"""
+Ask the user to input the exercise, and set and rep scheme.
+"""
+
 def get_graphing_parameters():
-    
+
     exercise = raw_input('What exercise do you want to graph? \n')
     min_sets = raw_input('What are the minimum number of sets to graph? \n')
     rep_range = raw_input('What minimum rep range do you want to graph? \n')
 
     return exercise, int(min_sets), int(rep_range)
+
+"""
+Graph the user's progress.
+"""
+
+def graph_progress(temporary_dates, temporary_weights, names, place_holder):
+
+    temporary_dates = [dt.datetime.strptime(index,'%m/%d/%Y').date() for index in temporary_dates]
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+
+    plt.plot(temporary_dates, temporary_weights)
+    plt.gcf().autofmt_xdate()
+
+    plt.xlabel('Time')
+    plt.ylabel('Weight (lbs)')
+    plt.title(names[place_holder])
+
+    plt.show()
 
 def main():
 
@@ -131,20 +173,7 @@ def main():
             del temporary_dates[index]
             del temporary_weights[index]
 
-    # Time to start graphing everything
-
-    temporary_dates = [dt.datetime.strptime(index,'%m/%d/%Y').date() for index in temporary_dates]
-
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-
-    plt.plot(temporary_dates, temporary_weights)
-    plt.gcf().autofmt_xdate()
-
-    plt.xlabel('Time')
-    plt.ylabel('Weight (lbs)')
-    plt.title(names[place_holder]) 
-
-    plt.show()
+    graph_progress(temporary_dates, temporary_weights, names, place_holder)
 
 if __name__ == "__main__":
     main()
